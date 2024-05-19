54b7a30a120f569645f7ad5ff65431555da5d261
AbdellahChalouli <abdellahchalouli@gmail.com>
2024-05-19 16:11:02 +0100

    Delete src/projet_poo/MainG.java

--- src/projet_poo/MainG.java
+++
@@ -1,8 +0,0 @@
-package projet_poo;
-
-public class MainG{
-    public static void main(String[] args) {
-        GestionRendezVous gestionRendezVous = new GestionRendezVous();
-        gestionRendezVous.lancerApplication();
-    }
-    }
